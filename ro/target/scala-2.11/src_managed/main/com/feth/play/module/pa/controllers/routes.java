129cf88498698d8f0e61c42f54e09ddd37a7de48
lycantropus <mechanical.maggot.0@gmail.com>
2016-06-12 01:41:01 +0100

    logins e conexao com o mongo

---
+++ ro/target/scala-2.11/src_managed/main/com/feth/play/module/pa/controllers/routes.java
@@ -0,0 +1,20 @@
+// @SOURCE:D:/roplay/ro/conf/routes
+// @HASH:aa6c8ad38cd24380f7b04de6f8a8031b8811f52e
+// @DATE:Sun Jun 12 01:25:54 BST 2016
+
+package com.feth.play.module.pa.controllers;
+
+public class routes {
+public static final com.feth.play.module.pa.controllers.ReverseAuthenticate Authenticate = new com.feth.play.module.pa.controllers.ReverseAuthenticate();
+
+public static class javascript {
+public static final com.feth.play.module.pa.controllers.javascript.ReverseAuthenticate Authenticate = new com.feth.play.module.pa.controllers.javascript.ReverseAuthenticate();
+}
+          
+
+public static class ref {
+public static final com.feth.play.module.pa.controllers.ref.ReverseAuthenticate Authenticate = new com.feth.play.module.pa.controllers.ref.ReverseAuthenticate();
+}
+          
+}
+          
---
+++ ro/target/scala-2.11/src_managed/main/controllers/routes.java
@@ -0,0 +1,29 @@
+// @SOURCE:D:/roplay/ro/conf/routes
+// @HASH:aa6c8ad38cd24380f7b04de6f8a8031b8811f52e
+// @DATE:Sun Jun 12 01:25:54 BST 2016
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseSignup Signup = new controllers.ReverseSignup();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseAccount Account = new controllers.ReverseAccount();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+
+public static class javascript {
+public static final controllers.javascript.ReverseSignup Signup = new controllers.javascript.ReverseSignup();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseAccount Account = new controllers.javascript.ReverseAccount();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReverseSignup Signup = new controllers.ref.ReverseSignup();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseAccount Account = new controllers.ref.ReverseAccount();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+          
+}
+          
