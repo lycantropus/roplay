c0cc0f12c082228376236c42f3aa0e9c508987c5
lycantropus <mechanical.maggot.0@gmail.com>
2016-06-13 07:56:05 +0100

    pre dep

--- ro/target/scala-2.11/twirl/main/views/html/restricted.template.scala
+++ ro/target/scala-2.11/twirl/main/views/html/restricted.template.scala
@@ -41,8 +41,8 @@ Seq[Any](format.raw/*1.51*/("""
         <input type="submit">
 
     </form>
-    <!--TODO tentar passar o name pelo cabeçalho da view-->
-    <table class="table" style="width:80%">
+
+    <table class="table" style="width:100%">
         <thead>
             <tr>
                 <th>Ro Name</th>
@@ -50,34 +50,45 @@ Seq[Any](format.raw/*1.51*/("""
             </tr>
         </thead>
 
-    <!--<td>"""),_display_(/*26.14*/localUser/*26.23*/.getIdentifier),format.raw/*26.37*/("""</td>-->
+
 
     """),_display_(/*28.6*/for(ro<-roList) yield /*28.21*/{_display_(Seq[Any](format.raw/*28.22*/("""
-        """),format.raw/*29.9*/("""<tr>
-            <td>"""),_display_(/*30.18*/ro/*30.20*/.getName),format.raw/*30.28*/("""</td>
-            <td>"""),_display_(/*31.18*/ro/*31.20*/.getOwner.getIdentifier),format.raw/*31.43*/("""</td>
-            """),_display_(/*32.14*/if(ro.getArtifacts!=null)/*32.39*/{_display_(Seq[Any](format.raw/*32.40*/("""
-
-
-            """),_display_(/*35.14*/for(artifact<-ro.getArtifacts) yield /*35.44*/{_display_(Seq[Any](format.raw/*35.45*/("""
-                    """),format.raw/*36.21*/("""<td>"""),_display_(/*36.26*/artifact/*36.34*/.getTitle),format.raw/*36.43*/("""</td>
-
-                """)))}),format.raw/*38.18*/("""
-            """)))}),format.raw/*39.14*/("""
-            """)))}),format.raw/*40.14*/("""
-
-
-
-            """),format.raw/*44.13*/("""<td>
-            """),_display_(/*45.14*/form(action = routes.Application.upload, 'enctype -> "multipart/form-data")/*45.89*/ {_display_(Seq[Any](format.raw/*45.91*/("""
-                """),format.raw/*46.17*/("""<input type="file" name="uploadfile" data-buttonText="Your label here.">
+        """),format.raw/*29.9*/("""<div>
+        <tr>
+            <td>"""),_display_(/*31.18*/ro/*31.20*/.getName),format.raw/*31.28*/("""</td>
+            <td>"""),_display_(/*32.18*/ro/*32.20*/.getOwner),format.raw/*32.29*/("""</td>
+<td>
+            """),_display_(/*34.14*/form(action = routes.Application.upload, 'enctype -> "multipart/form-data")/*34.89*/ {_display_(Seq[Any](format.raw/*34.91*/("""
+                """),format.raw/*35.17*/("""<input type="file" name="uploadfile" data-buttonText="Your label here.">
                 <input type="submit" value="Add">
-            """)))}),format.raw/*48.14*/("""
-            """),format.raw/*49.13*/("""</td>
-        </tr>
-    </table>
-    """)))}),format.raw/*52.6*/("""
-    """))}
+            """)))}),format.raw/*37.14*/("""
+
+"""),format.raw/*39.1*/("""</td>
+            """),_display_(/*40.14*/if(ro.getArtifacts!=null)/*40.39*/{_display_(Seq[Any](format.raw/*40.40*/("""
+
+
+
+
+            """),_display_(/*45.14*/for(artifact<-ro.getArtifacts) yield /*45.44*/{_display_(Seq[Any](format.raw/*45.45*/("""
+                    """),format.raw/*46.21*/("""<td>"""),_display_(/*46.26*/artifact/*46.34*/.getTitle),format.raw/*46.43*/("""</td>
+
+                """)))}),format.raw/*48.18*/("""
+            """)))}),format.raw/*49.14*/("""
+
+        """),format.raw/*51.9*/("""</tr>
+        </div>
+
+
+
+
+            """)))}),format.raw/*57.14*/("""
+    """),format.raw/*58.5*/("""</table>
+        """)))}),format.raw/*59.10*/("""
+
+
+
+
+"""))}
   }
 
   def render(localUser:models.User,roList:List[models.Ro]): play.twirl.api.HtmlFormat.Appendable = apply(localUser,roList)
@@ -89,11 +100,11 @@ Seq[Any](format.raw/*1.51*/("""
 }
               /*
                   -- GENERATED --
-                  DATE: Mon Jun 13 05:34:50 BST 2016
+                  DATE: Mon Jun 13 07:45:32 BST 2016
                   SOURCE: D:/roplay/ro/app/views/restricted.scala.html
-                  HASH: c33ae983501e0f3a0b837c75cdb1905661d161cd
-                  MATRIX: 749->1|934->50|962->103|989->105|1067->175|1106->177|1142->187|1173->192|1243->242|1320->292|1335->298|1376->318|1783->698|1801->707|1836->721|1877->736|1908->751|1947->752|1983->761|2032->783|2043->785|2072->793|2122->816|2133->818|2177->841|2223->860|2257->885|2296->886|2339->902|2385->932|2424->933|2473->954|2505->959|2522->967|2552->976|2607->1000|2652->1014|2697->1028|2741->1044|2786->1062|2870->1137|2910->1139|2955->1156|3122->1292|3163->1305|3231->1343
-                  LINES: 26->1|30->1|32->5|33->6|33->6|33->6|35->8|35->8|35->8|38->11|38->11|38->11|53->26|53->26|53->26|55->28|55->28|55->28|56->29|57->30|57->30|57->30|58->31|58->31|58->31|59->32|59->32|59->32|62->35|62->35|62->35|63->36|63->36|63->36|63->36|65->38|66->39|67->40|71->44|72->45|72->45|72->45|73->46|75->48|76->49|79->52
+                  HASH: a8739aff4e9fca7254dd848aea6e615f5d9268b2
+                  MATRIX: 749->1|934->50|962->103|989->105|1067->175|1106->177|1142->187|1173->192|1243->242|1320->292|1335->298|1376->318|1718->634|1749->649|1788->650|1824->659|1887->695|1898->697|1927->705|1977->728|1988->730|2018->739|2069->763|2153->838|2193->840|2238->857|2405->993|2434->995|2480->1014|2514->1039|2553->1040|2598->1058|2644->1088|2683->1089|2732->1110|2764->1115|2781->1123|2811->1132|2866->1156|2911->1170|2948->1180|3017->1218|3049->1223|3098->1241
+                  LINES: 26->1|30->1|32->5|33->6|33->6|33->6|35->8|35->8|35->8|38->11|38->11|38->11|55->28|55->28|55->28|56->29|58->31|58->31|58->31|59->32|59->32|59->32|61->34|61->34|61->34|62->35|64->37|66->39|67->40|67->40|67->40|72->45|72->45|72->45|73->46|73->46|73->46|73->46|75->48|76->49|78->51|84->57|85->58|86->59
                   -- GENERATED --
               */
           
